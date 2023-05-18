870bb7a290f8bea563aeb5eb490e3fcd3e08bc10
Mughees0 <abdul.mughees009@gmail.com>
2023-05-18 15:07:57 +0300

    working

--- clips/src/app/shared/shared.module.ts
+++ clips/src/app/shared/shared.module.ts
@@ -4,10 +4,21 @@ import { ModalComponent } from './modal/modal.component';
 import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 import { TabComponent } from './tab/tab.component';
 import { InputComponent } from './input/input.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [ModalComponent, TabsContainerComponent, TabComponent, InputComponent],
-  imports: [CommonModule],
-  exports: [ModalComponent, TabComponent, TabsContainerComponent ],
+  declarations: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    InputComponent,
+  ],
+  imports: [CommonModule, ReactiveFormsModule],
+  exports: [
+    ModalComponent,
+    TabComponent,
+    TabsContainerComponent,
+    InputComponent,
+  ],
 })
 export class SharedModule {}
