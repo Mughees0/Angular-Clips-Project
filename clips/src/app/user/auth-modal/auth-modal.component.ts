c076e31d9126f18d872b71438bc0f7d0903cbce1
Mughees0 <abdul.mughees009@gmail.com>
2023-05-18 05:18:47 +0300

    working

--- clips/src/app/user/auth-modal/auth-modal.component.ts
+++ clips/src/app/user/auth-modal/auth-modal.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-auth-modal',
   templateUrl: './auth-modal.component.html',
-  styleUrls: ['./auth-modal.component.css']
+  styleUrls: ['./auth-modal.component.css'],
 })
-export class AuthModalComponent {
-
-}
+export class AuthModalComponent {}
