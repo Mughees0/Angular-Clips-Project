2f773558fba60754177158dc293b297f410fba56
Mughees0 <abdul.mughees009@gmail.com>
2023-05-18 11:39:42 +0300

    working

--- clips/src/app/shared/shared.module.ts
+++ clips/src/app/shared/shared.module.ts
@@ -7,6 +7,6 @@ import { TabComponent } from './tab/tab.component';
 @NgModule({
   declarations: [ModalComponent, TabsContainerComponent, TabComponent],
   imports: [CommonModule],
-  exports: [ModalComponent],
+  exports: [ModalComponent, TabComponent, TabsContainerComponent ],
 })
 export class SharedModule {}
