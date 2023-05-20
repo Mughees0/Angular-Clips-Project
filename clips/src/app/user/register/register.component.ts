4aee5fd780343930822a75b1eeccea2969543f20
Mughees0 <abdul.mughees009@gmail.com>
2023-05-20 18:06:26 +0300

    working

--- clips/src/app/user/register/register.component.ts
+++ clips/src/app/user/register/register.component.ts
@@ -4,6 +4,7 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { AuthService } from 'src/app/services/auth.service';
 import IUser from 'src/app/models/user.model';
+import { RegisterValidators } from '../validators/register-validators';
 
 @Component({
   selector: 'app-register',
@@ -35,14 +36,17 @@ export class RegisterComponent {
   alertMsg = 'Please wait! Your account is being created';
   alertColor = 'blue';
 
-  registerForm = new FormGroup({
-    name: this.name,
-    email: this.email,
-    age: this.age,
-    password: this.password,
-    confirm_password: this.confirm_password,
-    phoneNumber: this.phoneNumber,
-  });
+  registerForm = new FormGroup(
+    {
+      name: this.name,
+      email: this.email,
+      age: this.age,
+      password: this.password,
+      confirm_password: this.confirm_password,
+      phoneNumber: this.phoneNumber,
+    },
+    [RegisterValidators.match('password', 'confirm_password')]
+  );
 
   async register() {
     this.showAlert = true;
---
+++ clips/src/app/user/validators/email-taken.ts
@@ -0,0 +1,7 @@
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
+export class EmailTaken {
+    constructor(private auth: AngularFireAuth) {
+      
+  }
+}
