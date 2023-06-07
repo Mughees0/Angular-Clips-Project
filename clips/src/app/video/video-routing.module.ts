013d17ed8962e03d334dfbd3eeba9a5e66f3e797
Mughees0 <abdul.mughees009@gmail.com>
2023-06-07 06:45:24 +0300

    routing comeplete

--- clips/src/app/video/video-routing.module.ts
+++ clips/src/app/video/video-routing.module.ts
@@ -2,20 +2,30 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ManageComponent } from './manage/manage.component';
 import { UploadComponent } from './upload/upload.component';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
 const routes: Routes = [
   {
     path: 'manage',
     component: ManageComponent,
     data: {
       authOnly: true,
+      authGuardPipe: () => {
+        return '/';
+      },
     },
+    canActivate: [AngularFireAuth],
   },
   {
     path: 'upload',
     component: UploadComponent,
     data: {
       authOnly: true,
+      authGuardPipe: () => {
+        return '/';
+      },
     },
+    canActivate: [AngularFireAuth],
   },
   {
     path: 'manage-clips',
