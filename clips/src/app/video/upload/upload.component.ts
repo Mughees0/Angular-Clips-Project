0aef623076728962a8201fd409e8fe70952f6242
Mughees0 <abdul.mughees009@gmail.com>
2023-06-08 13:09:29 +0300

    Handling edge cases for video uploading and adding database file fo ruplaod

--- clips/src/app/video/upload/upload.component.ts
+++ clips/src/app/video/upload/upload.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { AngularFireStorage } from '@angular/fire/compat/storage';
+import {
+  AngularFireStorage,
+  AngularFireUploadTask,
+} from '@angular/fire/compat/storage';
 import { v4 as uuid } from 'uuid';
+import { last, switchMap } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
+import { ClipService } from 'src/app/services/clip.service';
 
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
   styleUrls: ['./upload.component.css'],
 })
-export class UploadComponent {
+export class UploadComponent implements OnDestroy {
   isDragOver = false;
   file: File | null = null;
   nextStep = false;
@@ -16,13 +23,28 @@ export class UploadComponent {
   alertColor = 'blue';
   alertMessage = 'Please wait! Your video is uploading.';
   inSubmission = false;
+  percentComplete = 0;
+  showProgress = false;
+  user: firebase.User | null = null;
+  task?: AngularFireUploadTask | null = null;
 
   title = new FormControl('', {
     validators: [Validators.required, Validators.minLength(3)],
     nonNullable: true,
   });
 
-  constructor(private storage: AngularFireStorage) {}
+  constructor(
+    private storage: AngularFireStorage,
+    private auth: AngularFireAuth,
+    private clipService: ClipService
+  ) {
+    auth.user.subscribe((user) => {
+      this.user = user;
+    }); // get user
+  }
+  ngOnDestroy(): void {
+    this.task?.cancel();
+  }
 
   uploadForm = new FormGroup({
     title: this.title,
@@ -30,7 +52,9 @@ export class UploadComponent {
 
   storeFile($event: Event) {
     this.isDragOver = false;
-    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null;
+    this.file = ($event as DragEvent).dataTransfer
+      ? ($event as DragEvent).dataTransfer?.files.item(0) ?? null
+      : ($event.target as HTMLInputElement).files?.item(0) ?? null;
     if (!this.file || this.file.type !== 'video/mp4') {
       return;
     }
@@ -38,14 +62,49 @@ export class UploadComponent {
     this.nextStep = true;
   }
   uploadFile() {
+    this.uploadForm.disable();
     this.showAlert = true;
     this.alertColor = 'blue';
     this.alertMessage = 'Please wait! Your video is uploading.';
     this.inSubmission = true;
+    this.showProgress = true; // show progress bar
 
     const clipFileName = uuid();
     const clipPath = `clips/${clipFileName}.mp4`;
 
-    this.storage.upload(clipPath, this.file);
+    this.task = this.storage.upload(clipPath, this.file);
+    const clipRef = this.storage.ref(clipPath);
+
+    this.task.percentageChanges().subscribe((percentComplete) => {
+      this.percentComplete = (percentComplete as number) / 100;
+    });
+    this.task
+      .snapshotChanges()
+      .pipe(
+        last(),
+        switchMap(() => clipRef.getDownloadURL())
+      )
+      .subscribe({
+        next: (url) => {
+          const clip = {
+            uid: this.user?.uid as string,
+            displayName: this.user?.displayName as string,
+            title: this.title?.value,
+            fileName: `${clipFileName}.mp4`,
+            url,
+          };
+          this.clipService.createClip(clip);
+          this.alertColor = 'green';
+          this.alertMessage = 'Your video has been uploaded!';
+          this.showProgress = false; // hide progress bar
+        },
+        error: (err) => {
+          this.uploadForm.enable();
+          this.alertColor = 'red';
+          this.alertMessage = 'Your video could not be uploaded!';
+          this.inSubmission = true;
+          this.showProgress = false; // hide progress bar
+        },
+      });
   }
 }
